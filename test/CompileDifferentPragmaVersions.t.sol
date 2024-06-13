52da459c3b4073c515768728845360c957858ff7
Alex Collette <alexcollette@hotmail.com>
2024-06-13 18:19:04 +0200

    :art: small fix

--- test/CompileDifferentPragmaVersions.t.sol
+++ test/CompileDifferentPragmaVersions.t.sol
@@ -13,11 +13,11 @@ import { NonfungiblePositionManager } from
     "../lib/accounts-v2/lib/slipstream/contracts/periphery/NonfungiblePositionManager.sol";
 import { NonfungiblePositionManagerExtension } from
     "../lib/accounts-v2/test/utils/fixtures/uniswap-v3/extensions/NonfungiblePositionManagerExtension.sol";
+import { QuoterV2Extension } from "./utils/extensions/QuoterV2Extension.sol";
 import { QuoterV2 } from "../lib/accounts-v2/lib/swap-router-contracts/contracts/lens/QuoterV2.sol";
 import { SlipstreamQuoterV2Extension } from "./utils/extensions/SlipstreamQuoterV2Extension.sol";
 import { SwapRouter } from "../lib/accounts-v2/lib/slipstream/contracts/periphery/SwapRouter.sol";
 import { SwapRouter02 } from "../lib/accounts-v2/lib/swap-router-contracts/contracts/SwapRouter02.sol";
-import { QuoterV2Extension } from "./utils/extensions/QuoterV2Extension.sol";
 
 contract IsCompoundable_SlipstreamAutoCompoundHelper_Fuzz_Test is Test {
     function test() public { }
