ea6f053824a7a5e40a1db899142b9ba67b70887d
Thomas Smets <thomass-smets@hotmail.com>
2024-06-18 15:45:35 +0200

    🚀 add deploy script asset managers

--- script/utils/Constants.sol
+++ script/utils/Constants.sol
@@ -13,3 +13,9 @@ library CompounderHelpers {
     address constant SLIPSTREAM = address(0);
     address constant UNISWAP_V3 = address(0);
 }
+
+library Parameters {
+    uint256 constant COMPOUND_THRESHOLD = 5 * 1e18; // 5 USD
+    uint256 constant INITIATOR_SHARE = 0.01 * 1e18; // 1%
+    uint256 constant TOLERANCE = 0.005 * 1e18; // 0.5%
+}
