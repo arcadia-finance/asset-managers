b48e8e98a5564b47e18f0ea9b571dc86ed6367f0
Thomas Smets <thomass-smets@hotmail.com>
2024-07-25 14:19:03 +0200

    🚀 deploy compounders

--- script/utils/Constants.sol
+++ script/utils/Constants.sol
@@ -5,13 +5,13 @@
 pragma solidity 0.8.22;
 
 library Compounders {
-    address payable constant SLIPSTREAM = payable(address(0));
-    address constant UNISWAP_V3 = address(0);
+    address payable constant SLIPSTREAM = payable(address(0xccc601cFd309894ED7B8F15Cb35057E5A6a18B79));
+    address constant UNISWAP_V3 = address(0x351a4CE4C45029D847F396132953673BcdEAF324);
 }
 
 library CompounderHelpers {
-    address constant SLIPSTREAM = address(0);
-    address constant UNISWAP_V3 = address(0);
+    address constant SLIPSTREAM = address(0xAAAAA15c3E04E7a827aD60Ae0544588BfdaeBa61);
+    address constant UNISWAP_V3 = address(0x04Ecd9B27C2ab7bC26984135e0f6E82F7ff5014D);
 }
 
 library Parameters {
