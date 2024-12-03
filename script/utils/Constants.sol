05d576e98cef90d4a36b98e6564bd3fb860f774e
Thomas Smets <thomass-smets@hotmail.com>
2024-12-03 16:26:33 +0100

    🚀 deploy alien base compounder and new helpers

--- script/utils/Constants.sol
+++ script/utils/Constants.sol
@@ -5,17 +5,17 @@
 pragma solidity 0.8.22;
 
 library Compounders {
-    address constant ALIEN_BASE = address(0);
+    address constant ALIEN_BASE = address(0x15E755f17E3712F561d25538cCc0488445398c8D);
     address payable constant SLIPSTREAM = payable(address(0xccc601cFd309894ED7B8F15Cb35057E5A6a18B79));
     address constant UNISWAP_V3 = address(0x351a4CE4C45029D847F396132953673BcdEAF324);
 }
 
 library CompounderHelpers {
-    address constant ALIEN_BASE = address(0);
+    address constant ALIEN_BASE = address(0x8363503aD282d4D0B3742Befdce5b425c60F77E7);
     address constant SLIPSTREAM = address(0xAAAAA15c3E04E7a827aD60Ae0544588BfdaeBa61);
-    address constant SLIPSTREAM_V2 = address(0);
+    address constant SLIPSTREAM_V2 = address(0x8A44c068e90dDFf752dAd22E07BE2B71e5a98e11);
     address constant UNISWAP_V3 = address(0x04Ecd9B27C2ab7bC26984135e0f6E82F7ff5014D);
-    address constant UNISWAP_V3_V2 = address(0);
+    address constant UNISWAP_V3_V2 = address(0x58bc2000e0a3a8094C397B43e8621EF5dbA280e7);
 }
 
 library Parameters {
