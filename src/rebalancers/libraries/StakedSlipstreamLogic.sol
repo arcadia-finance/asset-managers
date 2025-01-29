49660b35a198ab5178e5ace4265e112dd28622c4
Alex Collette <alexcollette@hotmail.com>
2025-01-29 13:02:38 +0100

    :sparkles: add wrappedStakedSlipstream to Rebalancer logic

--- src/rebalancers/libraries/StakedSlipstreamLogic.sol
+++ src/rebalancers/libraries/StakedSlipstreamLogic.sol
@@ -4,13 +4,13 @@
  */
 pragma solidity 0.8.22;
 
-import { IStakedSlipstreamAM } from "../interfaces/IStakedSlipstreamAM.sol";
-
 library StakedSlipstreamLogic {
     // The contract address of the Reward Token (Aero).
     address internal constant REWARD_TOKEN = 0x940181a94A35A4569E4529A3CDfB74e38FD98631;
 
     // The Staked Slipstream Asset Module contract.
-    IStakedSlipstreamAM internal constant POSITION_MANAGER =
-        IStakedSlipstreamAM(0x1Dc7A0f5336F52724B650E39174cfcbbEdD67bF1);
+    address internal constant POSITION_MANAGER = 0x1Dc7A0f5336F52724B650E39174cfcbbEdD67bF1;
+
+    // The Wrapped Staked Slipstream Asset Module contract.
+    address internal constant POSITION_MANAGER_WRAPPED = address(0x00);
 }
