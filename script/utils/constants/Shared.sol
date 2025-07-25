ca74f567fd3746fdff4ea9a57e1bd21206c7de67
Thomas Smets <thomass-smets@hotmail.com>
2025-07-25 10:28:09 +0200

    🎨 forge fmt

--- script/utils/constants/Shared.sol
+++ script/utils/constants/Shared.sol
@@ -13,23 +13,23 @@ library ArcadiaAssetManagers {
 }
 
 library Compounders {
-    address constant SLIPSTREAM = 0x4694c34d153ee777cc07d01ac433bcc010a20ebd;
-    address constant UNISWAP_V3 = 0x80d3548bc54710d46201d554712e8638fd51326d;
-    address constant UNISWAP_V4 = 0xcff15e24a453afad454533e6d10889a84e2a68e1;
+    address constant SLIPSTREAM = 0x4694c34d153EE777CC07d01AC433bcC010A20EBd;
+    address constant UNISWAP_V3 = 0x80D3548bc54710d46201D554712E8638fD51326D;
+    address constant UNISWAP_V4 = 0xCfF15E24a453aFAd454533E6D10889A84e2A68e1;
 }
 
 library Helpers {
-    address constant ROUTER_TRAMPOLINE = 0x354dbba1348985cc952c467b8ddaf5dd07590667;
+    address constant ROUTER_TRAMPOLINE = 0x354dBBa1348985CC952c467b8ddaF5dD07590667;
 }
 
 library Rebalancers {
-    address constant SLIPSTREAM = 0xefe600366e9847d405f2238cf9196e33780b3a42;
-    address constant UNISWAP_V3 = 0xd8285fc23eff687b8b618b78d85052f1ed17236e;
-    address constant UNISWAP_V4 = 0xa8676c8c197e12a71ae82a08b02dd9e666312cf1;
+    address constant SLIPSTREAM = 0xEfe600366e9847D405f2238cF9196E33780B3A42;
+    address constant UNISWAP_V3 = 0xD8285fC23eFF687B8b618b78d85052f1eD17236E;
+    address constant UNISWAP_V4 = 0xa8676C8c197E12a71AE82a08B02DD9e666312cF1;
 }
 
 library YieldClaimers {
-    address constant SLIPSTREAM = 0x1f75abf8a24782053b351d9b4ea6d1236ed59105;
-    address constant UNISWAP_V3 = 0x40462e71effd9974fee04b6b327b701d663f753e;
-    address constant UNISWAP_V4 = 0x3bc2b398eeee9807ff76fdb4e11526de0ee80cea;
+    address constant SLIPSTREAM = 0x1f75aBF8a24782053B351D9b4EA6d1236ED59105;
+    address constant UNISWAP_V3 = 0x40462e71Effd9974Fee04B6b327B701D663f753e;
+    address constant UNISWAP_V4 = 0x3BC2B398eEEE9807ff76fdb4E11526dE0Ee80cEa;
 }
