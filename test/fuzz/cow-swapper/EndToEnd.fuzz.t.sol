f54883ce29f60874096a64bf80c964b477acdce3
Thomas Smets <thomass-smets@hotmail.com>
2025-09-30 15:05:43 +0200

    ♻️ refactor CowSwapper helper functions

--- test/fuzz/cow-swapper/EndToEnd.fuzz.t.sol
+++ test/fuzz/cow-swapper/EndToEnd.fuzz.t.sol
@@ -4,221 +4,86 @@
  */
 pragma solidity ^0.8.0;
 
-import { Borrower } from "../../../lib/flash-loan-router/src/mixin/Borrower.sol";
+import { Borrower } from "../../../src/cow-swapper/vendored/Borrower.sol";
 import { CowSwapper_Fuzz_Test } from "./_CowSwapper.fuzz.t.sol";
+import { ERC20Mock } from "../../../lib/accounts-v2/test/utils/mocks/tokens/ERC20Mock.sol";
 import { GPv2Order } from "../../../lib/cowprotocol/src/contracts/libraries/GPv2Order.sol";
 import { HooksTrampoline } from "../../utils/mocks/HooksTrampoline.sol";
 import { IBorrower } from "../../../lib/flash-loan-router/src/interface/IBorrower.sol";
 import { ICowSettlement } from "../../../lib/flash-loan-router/src/interface/ICowSettlement.sol";
 import { IERC20 } from "../../../lib/flash-loan-router/src/vendored/IERC20.sol";
 import { Loan } from "../../../lib/flash-loan-router/src/library/Loan.sol";
-import { RouterMock } from "../../../lib/accounts-v2/test/utils/mocks/action-targets/RouterMock.sol";
 
 /**
  * @notice Fuzz tests for the function full "flashLoanAndSettle" flow of contract "CowSwapper".
  */
 contract EndToEnd_CowSwapper_Fuzz_Test is CowSwapper_Fuzz_Test {
-    using GPv2Order for GPv2Order.Data;
-    /*////////////////////////////////////////////////////////////////
-                            VARIABLES
-    /////////////////////////////////////////////////////////////// */
-
-    RouterMock internal routerMock;
-
     /* ///////////////////////////////////////////////////////////////
                               SETUP
     /////////////////////////////////////////////////////////////// */
 
     function setUp() public override {
         CowSwapper_Fuzz_Test.setUp();
-
-        routerMock = new RouterMock();
     }
 
     /*//////////////////////////////////////////////////////////////
                               TESTS
     //////////////////////////////////////////////////////////////*/
-    function testFuzz_Revert_EndToEnd_Paused(address account_, InitiatorParams memory initiatorParams, address caller)
-        public
-    { }
+    function testFuzz_Revert_EndToEnd_Paused(
+        address account_,
+        uint64 swapFee,
+        GPv2Order.Data memory order,
+        address caller
+    ) public { }
 
-    function testFuzz_Success_EndToEnd(uint256 initiatorPrivateKey, uint96 amountIn, uint96 amountOut, uint64 swapFee)
+    function testFuzz_Success_EndToEnd(uint256 initiatorPrivateKey, uint64 swapFee, GPv2Order.Data memory order)
         public
     {
-        // Given: Valid input parameters.
-        // Private key must be less than the secp256k1 curve order and != 0
-        initiatorPrivateKey = bound(
-            initiatorPrivateKey,
-            1,
-            115_792_089_237_316_195_423_570_985_008_687_907_852_837_564_279_074_904_382_605_163_141_518_161_494_337 - 1
-        );
+        // Given: Valid initiator..
+        initiatorPrivateKey = givenValidPrivatekey(initiatorPrivateKey);
         address initiator = vm.addr(initiatorPrivateKey);
-        amountIn = uint96(bound(amountIn, 1, type(uint96).max));
-        amountOut = uint96(bound(amountOut, 1, type(uint96).max));
+
+        // And: Valid swap fee.
         swapFee = uint64(bound(swapFee, 0, MAX_FEE));
 
-        // And: An initiator is set.
-        {
-            address[] memory assetManagers = new address[](1);
-            assetManagers[0] = address(cowSwapper);
-            bool[] memory statuses = new bool[](1);
-            statuses[0] = true;
-            bytes[] memory datas = new bytes[](1);
-            datas[0] = bytes("");
-            vm.prank(users.accountOwner);
-            account.setAssetManagers(assetManagers, statuses, datas);
-        }
-        vm.prank(users.accountOwner);
-        cowSwapper.setAccountInfo(address(account), initiator, MAX_FEE, address(orderHook), abi.encode(""), "");
+        // And: Valid order.
+        givenValidOrder(order);
 
-        // And: Order is valid.
-        Loan.Data[] memory loans = new Loan.Data[](1);
-        loans[0] = Loan.Data({
-            amount: amountIn,
-            borrower: IBorrower(address(cowSwapper)),
-            lender: address(account),
-            token: IERC20(address(token0))
-        });
+        // And: Valid beforeSwap signature.
+        bytes memory initiatorSignature = getSignature(address(account), swapFee, order, initiatorPrivateKey);
 
-        ICowSettlement.Interaction[][3] memory interactions;
-        {
-            ICowSettlement.Interaction[] memory preInteractions = new ICowSettlement.Interaction[](2);
-            ICowSettlement.Interaction[] memory swapInteractions = new ICowSettlement.Interaction[](2);
-            ICowSettlement.Interaction[] memory postInteractions = new ICowSettlement.Interaction[](0);
-            interactions = [preInteractions, swapInteractions, postInteractions];
-        }
+        // And: Valid EIP-1271 signature.
+        bytes memory eip1271Signature = abi.encodePacked(address(cowSwapper), bytes(""));
 
-        interactions[0][0] = ICowSettlement.Interaction({
-            target: address(cowSwapper),
-            value: 0,
-            callData: abi.encodeCall(Borrower.approve, (IERC20(address(token0)), address(vaultRelayer), amountIn))
-        });
-
-        GPv2Order.Data memory order = GPv2Order.Data({
-            sellToken: token0_,
-            buyToken: token1_,
-            receiver: address(cowSwapper),
-            sellAmount: amountIn,
-            buyAmount: amountOut,
-            validTo: type(uint32).max,
-            appData: bytes32(0),
-            feeAmount: 0,
-            kind: GPv2Order.KIND_SELL,
-            partiallyFillable: false,
-            sellTokenBalance: GPv2Order.BALANCE_ERC20,
-            buyTokenBalance: GPv2Order.BALANCE_ERC20
-        });
-
-        // And: BeforeSwap is called in pre swap hook.
-        {
-            bytes memory signature = getSignature(address(account), swapFee, order, initiatorPrivateKey);
-            HooksTrampoline.Hook[] memory hooks = new HooksTrampoline.Hook[](1);
-            hooks[0] = HooksTrampoline.Hook({
-                target: address(cowSwapper),
-                callData: abi.encodeCall(cowSwapper.beforeSwap, (swapFee, order, signature)),
-                gasLimit: 40_000
-            });
-            interactions[0][1] = ICowSettlement.Interaction({
-                target: address(hooksTrampoline),
-                value: 0,
-                callData: abi.encodeCall(hooksTrampoline.execute, (hooks))
-            });
-        }
-
-        interactions[1][0] = ICowSettlement.Interaction({
-            target: address(token0),
-            value: 0,
-            callData: abi.encodeCall(token0.approve, (address(routerMock), amountIn))
-        });
-        interactions[1][1] = ICowSettlement.Interaction({
-            target: address(routerMock),
-            value: 0,
-            callData: abi.encodeCall(routerMock.swapAssets, (address(token0), address(token1), amountIn, amountOut))
-        });
-
-        // And: Solver correctly processes the order.
-        bytes memory settlement;
-        {
-            address[] memory tokens = new address[](2);
-            tokens[0] = address(token0);
-            tokens[1] = address(token1);
-
-            uint256[] memory clearingPrices = new uint256[](2);
-            clearingPrices[0] = order.buyAmount;
-            clearingPrices[1] = order.sellAmount;
-
-            ICowSettlement.Trade[] memory trades = new ICowSettlement.Trade[](1);
-            trades[0] = ICowSettlement.Trade(
-                0,
-                1,
-                order.receiver,
-                order.sellAmount,
-                order.buyAmount,
-                order.validTo,
-                order.appData,
-                order.feeAmount,
-                packFlags(),
-                order.sellAmount,
-                abi.encodePacked(address(cowSwapper), bytes(""))
-            );
-
-            settlement = abi.encodeCall(ICowSettlement.settle, (tokens, clearingPrices, trades, interactions));
-        }
+        // And: Cow swapper is set as asset manager with initiator.
+        setCowSwapper(initiator);
 
         // And: Account has sufficient tokenIn balance.
-        deal(address(token0), users.accountOwner, amountIn, true);
-        {
-            address[] memory assets_ = new address[](1);
-            uint256[] memory assetIds_ = new uint256[](1);
-            uint256[] memory assetAmounts_ = new uint256[](1);
-
-            assets_[0] = address(token0);
-            assetAmounts_[0] = amountIn;
-
-            // And : Deposit position in Account
-            vm.startPrank(users.accountOwner);
-            token0.approve(address(account), amountIn);
-            account.deposit(assets_, assetIds_, assetAmounts_);
-            vm.stopPrank();
-        }
+        depositErc20InAccount(account, ERC20Mock(address(order.sellToken)), order.sellAmount);
 
         // And: Router can successfully execute the swap.
-        deal(address(token1), address(routerMock), amountOut, true);
+        deal(address(order.buyToken), address(routerMock), order.buyAmount, true);
+
+        // And: Solver correctly processes the order.
+        Loan.Data[] memory loans = new Loan.Data[](1);
+        loans[0] = Loan.Data({
+            amount: order.sellAmount,
+            borrower: IBorrower(address(cowSwapper)),
+            lender: address(account),
+            token: IERC20(address(order.sellToken))
+        });
+
+        bytes memory settlementCallData = getSettlementCallData(swapFee, order, initiatorSignature, eip1271Signature);
 
         // When: The solver calls the flash loan router.
         vm.prank(solver);
-        flashLoanRouter.flashLoanAndSettle(loans, settlement);
+        flashLoanRouter.flashLoanAndSettle(loans, settlementCallData);
 
         // Then: Account has received the expected amount of tokenOut.
-        uint256 fee = uint256(amountOut) * swapFee / 1e18;
-        assertEq(token1.balanceOf(address(account)), amountOut - fee);
+        uint256 fee = uint256(order.buyAmount) * swapFee / 1e18;
+        assertEq(order.buyToken.balanceOf(address(account)), order.buyAmount - fee);
 
         // And: Initiator has received the expected fee.
-        assertEq(token1.balanceOf(initiator), fee);
-    }
-
-    function getSignature(address account_, uint256 swapFee, GPv2Order.Data memory order, uint256 privateKey)
-        public
-        view
-        returns (bytes memory sig)
-    {
-        bytes32 messageHash = keccak256(abi.encode(account_, swapFee, order.hash(cowSwapper.DOMAIN_SEPARATOR())));
-        sig = getSignature(messageHash, privateKey);
-    }
-
-    function getSignature(bytes32 messageHash, uint256 privateKey) public pure returns (bytes memory sig) {
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, messageHash);
-        return bytes.concat(r, s, bytes1(v));
-    }
-
-    function packFlags() internal pure returns (uint256) {
-        // For information on flag encoding, see:
-        // https://github.com/cowprotocol/contracts/blob/v1.0.0/src/contracts/libraries/GPv2Trade.sol#L70-L93
-        uint256 sellOrderFlag = 0;
-        uint256 fillOrKillFlag = 0 << 1;
-        uint256 internalSellTokenBalanceFlag = 0 << 2;
-        uint256 internalBuyTokenBalanceFlag = 0 << 4;
-        uint256 eip1271Flag = 2 << 5;
-        return sellOrderFlag | fillOrKillFlag | internalSellTokenBalanceFlag | internalBuyTokenBalanceFlag | eip1271Flag;
+        assertEq(order.buyToken.balanceOf(initiator), fee);
     }
 }
--- test/fuzz/cow-swapper/_CowSwapper.fuzz.t.sol
+++ test/fuzz/cow-swapper/_CowSwapper.fuzz.t.sol
@@ -11,13 +11,17 @@ import { DefaultOrderHook } from "../../utils/mocks/DefaultOrderHook.sol";
 import { ERC20Mock } from "../../../lib/accounts-v2/test/utils/mocks/tokens/ERC20Mock.sol";
 import { Fuzz_Test } from "../Fuzz.t.sol";
 import { GPv2Order } from "../../../lib/cowprotocol/src/contracts/libraries/GPv2Order.sol";
+import { HooksTrampoline } from "../../utils/mocks/HooksTrampoline.sol";
+import { ICowSettlement } from "../../../lib/flash-loan-router/src/interface/ICowSettlement.sol";
 import { IERC20 } from "../../../lib/cowprotocol/src/contracts/interfaces/IERC20.sol";
+import { RouterMock } from "../../../lib/accounts-v2/test/utils/mocks/action-targets/RouterMock.sol";
 import { WETH9Fixture } from "../../../lib/accounts-v2/test/utils/fixtures/weth9/WETH9Fixture.f.sol";
 
 /**
  * @notice Common logic needed by all "CowSwapper" fuzz tests.
  */
 abstract contract CowSwapper_Fuzz_Test is Fuzz_Test, BalancerV2Fixture, CowSwapFixture, WETH9Fixture {
+    using GPv2Order for GPv2Order.Data;
     /*////////////////////////////////////////////////////////////////
                             CONSTANTS
     /////////////////////////////////////////////////////////////// */
@@ -31,9 +35,7 @@ abstract contract CowSwapper_Fuzz_Test is Fuzz_Test, BalancerV2Fixture, CowSwapF
     ERC20Mock internal token0;
     ERC20Mock internal token1;
 
-    // Need to declare it here since cowprotocol and flash-loan-router use different versions of IERC20.
-    IERC20 internal token0_;
-    IERC20 internal token1_;
+    RouterMock internal routerMock;
 
     struct InitiatorParams {
         uint64 swapFee;
@@ -72,8 +74,6 @@ abstract contract CowSwapper_Fuzz_Test is Fuzz_Test, BalancerV2Fixture, CowSwapF
         // Create tokens.
         token0 = new ERC20Mock("TokenA", "TOKA", 0);
         token1 = new ERC20Mock("TokenB", "TOKB", 0);
-        token0_ = IERC20(address(token0));
-        token1_ = IERC20(address(token1));
         addAssetToArcadia(address(token0), int256(1e18));
         addAssetToArcadia(address(token1), int256(1e18));
 
@@ -83,11 +83,175 @@ abstract contract CowSwapper_Fuzz_Test is Fuzz_Test, BalancerV2Fixture, CowSwapF
 
         // Deploy mocked order hook.
         orderHook = new DefaultOrderHook();
+
+        // Deploy mocked router.
+        routerMock = new RouterMock();
     }
 
     /*//////////////////////////////////////////////////////////////////////////
                                 HELPER FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*/
 
-    function test_dep() public { }
+    function givenValidPrivatekey(uint256 privateKey) internal pure returns (uint256) {
+        // Private key must be less than the secp256k1 curve order and != 0
+        return bound(
+            privateKey,
+            1,
+            115_792_089_237_316_195_423_570_985_008_687_907_852_837_564_279_074_904_382_605_163_141_518_161_494_337 - 1
+        );
+    }
+
+    function givenValidOrder(GPv2Order.Data memory order) internal view {
+        order.sellToken = IERC20(address(token0));
+        order.buyToken = IERC20(address(token1));
+        order.receiver = address(cowSwapper);
+        order.sellAmount = uint96(bound(order.sellAmount, 1, type(uint96).max));
+        order.buyAmount = uint96(bound(order.buyAmount, 1, type(uint96).max));
+        order.validTo = uint32(bound(order.validTo, block.timestamp, type(uint32).max));
+        order.feeAmount = 0;
+        order.kind = GPv2Order.KIND_SELL;
+        order.partiallyFillable = false;
+        order.sellTokenBalance = GPv2Order.BALANCE_ERC20;
+        order.buyTokenBalance = GPv2Order.BALANCE_ERC20;
+    }
+
+    function setCowSwapper(address initiator) internal {
+        // Given: Cow Swapper is set a s Asset Manager.
+        address[] memory assetManagers = new address[](1);
+        assetManagers[0] = address(cowSwapper);
+        bool[] memory statuses = new bool[](1);
+        statuses[0] = true;
+        bytes[] memory datas = new bytes[](1);
+        datas[0] = bytes("");
+        vm.prank(users.accountOwner);
+        account.setAssetManagers(assetManagers, statuses, datas);
+
+        // And: Initiator is set on Cow Swapper.
+        vm.prank(users.accountOwner);
+        cowSwapper.setAccountInfo(address(account), initiator, MAX_FEE, address(orderHook), abi.encode(""), "");
+    }
+
+    function getSettlementCallData(
+        uint64 swapFee,
+        GPv2Order.Data memory order,
+        bytes memory initiatorSignature,
+        bytes memory eip1271Signature
+    ) internal view returns (bytes memory settlementCallData) {
+        (
+            address[] memory tokens,
+            uint256[] memory clearingPrices,
+            ICowSettlement.Trade[] memory trades,
+            ICowSettlement.Interaction[][3] memory interactions
+        ) = getSettlementData(swapFee, order, initiatorSignature, eip1271Signature);
+
+        settlementCallData = abi.encodeCall(ICowSettlement.settle, (tokens, clearingPrices, trades, interactions));
+    }
+
+    function getSettlementData(
+        uint64 swapFee,
+        GPv2Order.Data memory order,
+        bytes memory initiatorSignature,
+        bytes memory eip1271Signature
+    )
+        internal
+        view
+        returns (
+            address[] memory tokens,
+            uint256[] memory clearingPrices,
+            ICowSettlement.Trade[] memory trades,
+            ICowSettlement.Interaction[][3] memory interactions
+        )
+    {
+        tokens = new address[](2);
+        tokens[0] = address(order.sellToken);
+        tokens[1] = address(order.buyToken);
+
+        clearingPrices = new uint256[](2);
+        clearingPrices[0] = order.buyAmount;
+        clearingPrices[1] = order.sellAmount;
+
+        trades = new ICowSettlement.Trade[](1);
+        trades[0] = ICowSettlement.Trade(
+            0,
+            1,
+            order.receiver,
+            order.sellAmount,
+            order.buyAmount,
+            order.validTo,
+            order.appData,
+            order.feeAmount,
+            packFlags(),
+            order.sellAmount,
+            eip1271Signature
+        );
+
+        // Pre swap interactions.
+        interactions[0] = new ICowSettlement.Interaction[](2);
+        // Driver interaction.
+        interactions[0][0] = ICowSettlement.Interaction({
+            target: address(cowSwapper),
+            value: 0,
+            callData: abi.encodeWithSignature(
+                "approve(address,address,uint256)", address(order.sellToken), address(vaultRelayer), order.sellAmount
+            )
+        });
+
+        // And: BeforeSwap is called in pre swap hook.
+        {
+            HooksTrampoline.Hook[] memory hooks = new HooksTrampoline.Hook[](1);
+            hooks[0] = HooksTrampoline.Hook({
+                target: address(cowSwapper),
+                callData: abi.encodeCall(cowSwapper.beforeSwap, (swapFee, order, initiatorSignature)),
+                gasLimit: 40_000
+            });
+            interactions[0][1] = ICowSettlement.Interaction({
+                target: address(hooksTrampoline),
+                value: 0,
+                callData: abi.encodeCall(hooksTrampoline.execute, (hooks))
+            });
+        }
+
+        // Swap interactions.
+        interactions[1] = new ICowSettlement.Interaction[](2);
+        interactions[1][0] = ICowSettlement.Interaction({
+            target: address(order.sellToken),
+            value: 0,
+            callData: abi.encodeCall(token0.approve, (address(routerMock), order.sellAmount))
+        });
+        interactions[1][1] = ICowSettlement.Interaction({
+            target: address(routerMock),
+            value: 0,
+            callData: abi.encodeCall(
+                routerMock.swapAssets,
+                (address(order.sellToken), address(order.buyToken), order.sellAmount, order.buyAmount)
+            )
+        });
+
+        // No Post Swap interactions.
+    }
+
+    function packFlags() internal pure returns (uint256) {
+        // For information on flag encoding, see:
+        // https://github.com/cowprotocol/contracts/blob/v1.0.0/src/contracts/libraries/GPv2Trade.sol#L70-L93
+        uint256 sellOrderFlag = 0;
+        uint256 fillOrKillFlag = 0 << 1;
+        uint256 internalSellTokenBalanceFlag = 0 << 2;
+        uint256 internalBuyTokenBalanceFlag = 0 << 4;
+        uint256 eip1271Flag = 2 << 5;
+        return sellOrderFlag | fillOrKillFlag | internalSellTokenBalanceFlag | internalBuyTokenBalanceFlag | eip1271Flag;
+    }
+
+    function getSignature(address account_, uint256 swapFee, GPv2Order.Data memory order, uint256 privateKey)
+        public
+        view
+        returns (bytes memory sig)
+    {
+        bytes32 messageHash = keccak256(abi.encode(account_, swapFee, order.hash(cowSwapper.DOMAIN_SEPARATOR())));
+        sig = getSignature(messageHash, privateKey);
+    }
+
+    function getSignature(bytes32 messageHash, uint256 privateKey) public pure returns (bytes memory sig) {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, messageHash);
+        return bytes.concat(r, s, bytes1(v));
+    }
 }
